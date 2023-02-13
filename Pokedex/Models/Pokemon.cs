48bfc950d1370f279565e372edda30ced243183c
Maria Luiza <marialuizagon1008@gmail.com>
2023-02-13 11:28:23 -0300

    Adicionado os arquivos estáticos (css, imaggens e json). Criada as classes de models:Tipo e Pokemon

---
+++ Pokedex/Models/Pokemon.cs
@@ -0,0 +1,21 @@
+namespace Pokedex.Models
+{
+    public class Pokemon
+    {
+        //Atributos
+        public int Numero {get; set;}
+        public int Nome {get; set;}
+        public string Descricao {get; set;}
+        public string Especie { get; set; }
+        public List<string> Tipo { get; set; }
+        public double Altura { get; set; }
+        public double Peso  { get; set; }
+        public string Imagem { get; set; }
+
+        //Método Construtor
+        public Pokemon()
+        {
+            Tipo = new List<string>();
+        }
+    }
+}
